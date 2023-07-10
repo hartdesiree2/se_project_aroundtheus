6ad7fe8ddccfb3846d611ff22a799a7123a57b67
Desiree Hart <desiree_hart2@aol.com>
2023-07-10 14:52:49 -0500

    P4 Stage1

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
 const initialCards = [
 {
-    title: "Yosemite Valley",
+    card_title: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
 },
 
 {
-    title: "Lake Louise",
+    card_title: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
 },
 
 {
-    title: "Bald Mountains",
+    card_title: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
 },
 
 {
-    title: "Latemar",
+    card_title: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
 },
 
 {
-    title: "Vanoise National Park",
+    card_title: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
 },
 
 {
-    title: "Lago di Braies",
+    card_title: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
 }
 
